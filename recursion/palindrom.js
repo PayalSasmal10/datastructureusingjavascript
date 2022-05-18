324378d352508975ea8916bf56f360c45e041b99
Payal Sasmal <sasmalpayal@gmail.com>
2022-05-18 18:54:10 +0530

    recursion palindrome done

---
+++ recursion/palindrom.js
@@ -0,0 +1,15 @@
+let palin_str = "acbba"
+
+function palindrom(strr, i, j){
+    if (i >= j){
+        return true
+    }
+
+    if (strr[i] != strr[j]){
+        return false
+    }
+
+    return palindrom(strr, i+1, j-1)
+}
+
+console.log(palindrom(palin_str, 0, palin_str.length-1))
